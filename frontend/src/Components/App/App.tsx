4921f79f2d47363a680641d0c89f41aa8a9197e9
Fernando Asulay <fernando.asulay@gmail.com>
2022-01-30 18:02:23 +0000

    Added frontend

---
+++ frontend/src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '..';
+
+export function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>asd</h1>
+    </div>
+  );
+}
+
