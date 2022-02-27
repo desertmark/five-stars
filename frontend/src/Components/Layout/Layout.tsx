e8d43b5f471050c1e10e00722c0e3b94c369ca0c
Fernando Asulay <fernando.asulay@gmail.com>
2022-02-27 10:18:06 +0000

    Fix layout colors

--- frontend/src/Components/Layout/Layout.tsx
+++ frontend/src/Components/Layout/Layout.tsx
@@ -10,7 +10,7 @@ export const Layout: React.FC = ({ children }) => {
         <Box
           data-id="layout"
           sx={{
-            backgroundColor: "#333",
+            backgroundColor: "#ccc",
             width: "100%",
             height: "100%",
             display: "flex",
